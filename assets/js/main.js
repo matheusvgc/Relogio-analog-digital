2d54188d120ef1154e5b099af8026029d1228b0d
Matheus <mviniciuscardoso546@gmail.com>
2022-02-22 00:45:57 -0300

    adicionando relogio digital

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,75 @@ const clock = () => {
 }
 
 setInterval(clock, 1000)
+
+//=============== CLOCK AND DATE TEXT ==============//
+
+const textHour = document.getElementById('text-hour'),
+  textMinutes = document.getElementById('text-minutes'),
+  textAmPm = document.getElementById('text-ampm'),
+  dateDay = document.getElementById('date-day'),
+  dateMonth = document.getElementById('date-month'),
+  dateYear = document.getElementById('date-year')
+
+const clockText = () => {
+  let date = new Date()
+
+  let hh = date.getHours(),
+    ampm,
+    mm = date.getMinutes(),
+    day = date.getDate(),
+    month = date.getMonth(),
+    year = date.getFullYear()
+
+  //ampm
+  if (hh >= 12) {
+    hh = hh - 12
+    ampm = 'PM'
+  } else {
+    ampm = 'AM'
+  }
+
+  if (hh == 0) {
+    hh = 12
+  }
+
+  //zeros
+  if (hh < 10) {
+    hh = `0${hh}`
+  }
+  if (mm < 10) {
+    mm = `0${mm}`
+  }
+
+  // show time
+  textHour.innerHTML = `${hh}:`
+
+  // show minutes
+  textMinutes.innerHTML = `${mm}`
+
+  // show ampm
+  textAmPm.innerHTML = ampm
+
+  //months
+  let months = [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Maio',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez'
+  ]
+
+  // show month day and year
+  dateDay.innerHTML = day
+  dateMonth.innerHTML = `${months[month]},`
+  dateYear.innerHTML = year
+}
+
+setInterval(clockText, 1000)
